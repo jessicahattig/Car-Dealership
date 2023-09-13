14d7ee9cc24810f2881512c8a81e917bd57214e1
jessicahattig <jessicahattig@gmail.com>
2023-09-12 18:59:14 -0700

    dotnet build project.

---
+++ CarDealership/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FriendLetter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+      builder.Services.AddControllersWithViews();
+      WebApplication app = builder.Build();
+      app.UseDeveloperExceptionPage();
+      app.UseRouting();
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+      app.Run();
+    }
+  }
+}
