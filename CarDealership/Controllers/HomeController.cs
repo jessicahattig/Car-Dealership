04658d595397ab1315df6ca44d5396e6592c924b
jessicahattig <jessicahattig@gmail.com>
2023-09-12 19:12:10 -0700

    BUGFIX: Model references.

---
+++ CarDealership/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
+
+namespace CarDealership.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
--- CarDealership/Models/Item.cs
+++ CarDealership/Models/Item.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-namespace ToDoList.Models
+namespace CarDealership.Models
 {
   public class Item
   {
